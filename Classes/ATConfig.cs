09877aaea4d036ae42463d0cedd9c875ed3814f7
itLezy <itLezy@users.noreply.github.com>
2022-05-24 19:30:02 +0200

    OK

---
+++ Classes/ATConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchiveTorrents
+{
+    class ATConfig
+    {
+        // The timeout will be determined by TORRENT_PARALLEL_LIMIT * MAIN_LOOP_INTERVAL as torrents get removed on a FIFO logic basis
+
+        public readonly String TORR_EXT_WILDCARD = "*.torrent";
+        public readonly String TORR_ARCHIVE_DIR = @"x:\torr_archived\arc\";
+        public readonly String TORR_ARCHIVE_DIR_OLD = @"x:\torr_archived\arc\old\";
+        public readonly String TORR_ARCHIVE_REG = @"..\!!!!_torrs.lst";
+        public readonly String TORR_ARCHIVE_FILES_REG = @"..\!!!!_torrs_files.lst";
+        public readonly String TORR_INCOMING_DIR = @"x:\torr_incoming\";
+
+        public readonly String TORR_INPUT_DIR = Environment.GetFolderPath (Environment.SpecialFolder.UserProfile) + @"\Downloads";
+
+        public readonly string SDB_DLD_URL = ConfigurationManager.AppSettings["SDB_DLD_URL"];
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,153 +5,60 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 
-using MonoTorrent;
+using ILCommon;
 
-using static Crayon.Output;
+using MonoTorrent;
+using CommandLine;
+
+using ILCommon.Model;
 
 namespace ArchiveTorrents
 {
     class Program
     {
-        private const String TORR_EXT_WILDCARD = "*.torrent";
-        private const String TORR_ARCHIVE_DIR = @"x:\torr_archived\arc\";
-        private const String TORR_ARCHIVE_DIR_OLD = TORR_ARCHIVE_DIR + @"old\";
-        private const String TORR_ARCHIVE_REG = @"..\!!!!_torrs.lst";
-        private const String TORR_ARCHIVE_FILES_REG = @"..\!!!!_torrs_files.lst";
-        private const String TORR_INCOMING_DIR = @"x:\torr_incoming\";
-
-        private static String TORR_INPUT_DIR = Environment.GetFolderPath (Environment.SpecialFolder.UserProfile) + @"\Downloads";
+        static LockManager lockManager = new LockManager ();
 
         static void Main (String[] args)
         {
-            if (args.Length <= 0) {
-                RemDupsAndArchive ();
+            try {
+                if (lockManager.AcquireLock ()) {
+
+                    CommandLine.Parser.Default.ParseArguments<CommandLineOptions> (args)
+                        .WithParsed (RunOptions)
+                        .WithNotParsed (HandleParseError);
+
+                }
+
+            } finally {
+                lockManager.ReleaseLock ();
+            }
+
+
+        }
+
+        static void RunOptions (CommandLineOptions opts)
+        {
+            if (opts.CreateTables) {
+
+                new DAO ().CreateTables ();
+            } else if (opts.LoadDownloadedTorrents && Directory.Exists (opts.InputDir)) {
+
+                new ArchiveManager ().LoadDownloadedTorrents (opts.InputDir);
+            } else if (opts.LoadDownloadedFiles && Directory.Exists (opts.InputDir)) {
+
+                new ArchiveManager ().LoadDownloadedFiles (opts.InputDir);
             } else {
-                if ("l".Equals (args[0])) {
-                    ListFilesAndLen (args[1]);
-                }
+
+                new ArchiveManager ().RemDupsAndArchive ();
             }
         }
 
-        private static void ListFilesAndLen (String inputDir)
+        static void HandleParseError (IEnumerable<Error> errs)
         {
-            // generate the index of files + "|" + size, so to skip dups of files we have no torrent for
-            const int MB_10 = 1024 * 1024 * 10;
-
-            Console.Error.WriteLine ($"Processing Input Directory [{ Green (inputDir)}], looking for files gt than 10Mb..");
-
-            // find files that have been downloaded multiple times, and delete them like "abc (1).torrent"
-            var allFiles = Directory.GetFiles (inputDir, "*.*", SearchOption.AllDirectories);
-
-            for (var i = 0; i < allFiles.Length; i++) {
-                try {
-                    var file = new FileInfo (allFiles[i]);
-
-                    if (file.Length > MB_10 &&
-                        !file.Name.EndsWith ("!qB")) {
-
-                        Console.WriteLine ($"{file.Name + "|" + file.Length}");
-                    }
-
-                } catch (System.IO.FileNotFoundException ex) {
-                    Console.Error.WriteLine ("File name too long {0}", ex.Message);
-                }
-            }
+            //handle errors
+            Console.Error.WriteLine ("Command line nope!");
         }
 
-        private static void RemDupsAndArchive ()
-        {
-            Console.WriteLine ($"Processing Input Directory [{ Green (TORR_INPUT_DIR)}], looking for duplicates..");
 
-            // find files that have been downloaded multiple times, and delete them like "abc (1).torrent"
-            var dupTorrFiles = Directory.GetFiles (TORR_INPUT_DIR, "* (?).torrent");
-
-            for (var i = 0; i < dupTorrFiles.Length; i++) {
-                var torrFile = new FileInfo (dupTorrFiles[i]);
-
-                Console.WriteLine ($"Found multiple-download file {Red (torrFile.Name)}");
-
-                File.Delete (torrFile.FullName);
-            }
-
-            Console.WriteLine ();
-            Console.WriteLine ($"Processing Input Directory [{ Green (TORR_INPUT_DIR)}], processing torrent files..");
-            Console.WriteLine ();
-
-            // find actual torrent files
-            var torrFiles = Directory.GetFiles (TORR_INPUT_DIR, TORR_EXT_WILDCARD);
-            var duplicatesCount = 0;
-            var copiedCount = 0;
-            var totalFiles = torrFiles.Length;
-
-            for (var i = 0; i < torrFiles.Length; i++) {
-                var torrFile = new FileInfo (torrFiles[i]);
-
-                //Console.WriteLine ($"Found file        [{ Magenta (torrFile.Name) }]");
-
-                var torrTorr = Torrent.Load (torrFile.FullName);
-                var torrLargestFile = torrTorr.Files.OrderByDescending (t => t.Length).First ();
-                var torrHashId = torrTorr.InfoHashes.V1OrV2.ToHex ().ToLower ();
-                var normalizedName = Path.GetFileNameWithoutExtension (normalizeFileName (torrFile.Name));
-
-                Console.WriteLine ($"Found file        [{ Magenta (torrFile.Name) }], hashId { Green (torrHashId) }");
-                Console.WriteLine ($"             >    [{ Magenta (torrLargestFile.Path) }], size { Green (torrLargestFile.Length.ToString ()) } ");
-
-                if (File.ReadLines (TORR_ARCHIVE_DIR + TORR_ARCHIVE_REG).Contains (torrHashId)) {
-                    // remove duplicate if the same hashId was already in the list
-                    Console.WriteLine ($"Duplicate found L [{ Red (torrFile.Name) }], removing..");
-                    duplicatesCount++;
-                } else if (File.ReadLines (TORR_ARCHIVE_DIR + TORR_ARCHIVE_FILES_REG).Contains (torrLargestFile.Path + "|" + torrLargestFile.Length)) {
-                    // remove duplicate if the same file with the same exact length was already in the list
-                    Console.WriteLine ($"Duplicate found R [{ Red (torrFile.Name) }], removing..");
-                    duplicatesCount++;
-                } else if (
-                    Directory.GetFiles (TORR_ARCHIVE_DIR, normalizedName + TORR_EXT_WILDCARD).Length > 0 ||
-                    Directory.GetFiles (TORR_ARCHIVE_DIR_OLD, normalizedName + TORR_EXT_WILDCARD).Length > 0
-                    ) {
-                    // remove duplicate if the same torrent file exists
-                    Console.WriteLine ($"Duplicate found F [{ Red (torrFile.Name) }], removing..");
-                    duplicatesCount++;
-                } else {
-                    Console.WriteLine ($"Archiving torrent [{ Green (torrFile.Name) }]");
-
-                    // archive as copy
-                    File.Copy (
-                                torrFile.FullName,
-                                TORR_ARCHIVE_DIR + torrFile.Name
-                                );
-
-                    // copy to incoming folder of torrent client to pick up
-                    File.Copy (
-                                torrFile.FullName,
-                                TORR_INCOMING_DIR + torrFile.Name
-                                );
-
-                    // add the hashId to the list, so to be sure we can detect duplicates even if the file-name differs
-                    File.AppendAllLines (TORR_ARCHIVE_DIR + TORR_ARCHIVE_REG, new String[] { torrHashId });
-                    // add the largest file name and size to the list, so to be sure we can detect duplicates even if the file-name differs or it's the same file in different torrent files
-                    File.AppendAllLines (TORR_ARCHIVE_DIR + TORR_ARCHIVE_FILES_REG, new String[] { torrLargestFile.Path + "|" + torrLargestFile.Length });
-
-                    copiedCount++;
-                }
-
-                // delete original file at the end
-                File.Delete (torrFile.FullName);
-                Console.WriteLine ();
-            }
-
-            Console.WriteLine ();
-            Console.WriteLine ($"{Green ("It's all good man.") } Duplicates { duplicatesCount }, copied { copiedCount } out of { totalFiles } ");
-
-            //Thread.Sleep (4000);
-            Console.ReadLine ();
-        }
-
-        private static String normalizeFileName (String originalName)
-        {
-            return originalName
-                .Replace ("-[rarbg.to]", "")
-                .Replace ("[rarbg]", "");
-        }
     }
 }
